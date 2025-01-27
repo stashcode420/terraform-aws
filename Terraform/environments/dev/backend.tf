fb65192d6e3514e1985bd8ab5284b35a91e1b9f7
Tahir Javed <tahirjavedbhutta@gmail.com>
2025-01-27 17:40:15 +0500

    Added everything

--- Terraform/environments/dev/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "trading-platform-terraform-dev"
-    key            = "dev/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-state-lock-dev"
-    encrypt        = true
-  }
-}
